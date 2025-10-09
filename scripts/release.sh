c4f4ca4d1f2915c9bfd0afabe76f00aa377989b0
Omid Mufeed <omid@omidmufeed.com>
2025-10-09 13:01:19 +0100

    Release: Fix  typos in the comments

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,14 +8,14 @@
 
 set -e  # Exit on any error
 
-# Colors for output
+# Colours for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+NC='\033[0m' # No Colour
 
-# Function to print colored output
+# Function to print coloured output
 print_status() {
     echo -e "${BLUE}[INFO]${NC} $1"
 }
