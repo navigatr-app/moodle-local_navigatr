fa747602d4e34267f64ded83c0d1ca575c9e836c
Omid Mufeed <omid@omidmufeed.com>
2025-10-15 12:25:59 +0100

    Release 1.0.0
    - Updated version to 1.0.0
    - Updated changes with release notes
    - Created release branch for 1.0.0

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'local_navigatr';
 $plugin->requires  = 2022112800; // Moodle 4.1
 $plugin->version   = 2025010701; // YYYYMMDDXX
-$plugin->release   = '0.9.1';
+$plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [];
